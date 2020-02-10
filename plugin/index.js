437d6a85c34bfdefba2b02727925cb0cb56e3965
Suhan Moon <leader@trabricks.io>
2020-02-10 09:01:51 +0900

    1

--- plugin/index.js
+++ plugin/index.js
@@ -1,4 +1,5 @@
 import BabelInlineImportHelper from './helper';
+import transform from "css-to-react-native-transform";
 
 export default function({ types: t }) {
   class BabelInlineImport {
@@ -18,7 +19,7 @@ export default function({ types: t }) {
                 }
 
                 const id = path.node.specifiers[0].local.name;
-                const content = BabelInlineImportHelper.getContents(givenPath, reference);
+                const content = transform(BabelInlineImportHelper.getContents(givenPath, reference));
                 const variable = t.variableDeclarator(t.identifier(id), t.stringLiteral(content));
 
 		console.log(content);
